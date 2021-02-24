e13392269dba3e92270a4ba5ae9c265a963bb23b
kanno <kanno@fundely.co.jp>
2021-02-24 14:56:28 +0900

    screen

---
+++ src/components/InboxScreen.vue
@@ -0,0 +1,16 @@
+<!-- src/components/InboxScreen.vue -->
+
+<template>
+  <PureInboxScreen :error="error" />
+</template>
+
+<script>
+import PureInboxScreen from "./PureInboxScreen";
+import { mapState } from "vuex";
+
+export default {
+  name: "InboxScreen",
+  components: { PureInboxScreen },
+  computed: mapState(["error"]),
+};
+</script>
